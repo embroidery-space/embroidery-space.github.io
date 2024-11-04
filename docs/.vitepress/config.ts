b5971d01fbd14d946901ef29a6d25f9afe3e3000
Nazar Anroniuk <nazarantoniuk18@gmail.com>
2024-11-04 10:48:19 +0200

    initial commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "Embroider Space",
+  description: "A space for anyone interested in or engaged in embroidery.",
+  themeConfig: {
+    socialLinks: [
+      { icon: "github", link: "https://github.com/embroidery-space" },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+---
+layout: home
+hero:
+  name: Embroidery Space
+  tagline: A space for anyone interested in or engaged in embroidery.
+---
